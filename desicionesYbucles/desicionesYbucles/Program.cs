ab059fa9bed7c3f0978f6eb0afa026aedafc47b5
Eduardo0504 <maujohernav50@gmail.com>
2025-06-22 13:13:28 -0600

    first commit

---
+++ desicionesYbucles/desicionesYbucles/Program.cs
@@ -0,0 +1,28 @@
+﻿var cantidad1 = 5;
+var cantidad2 = 5;
+
+var cantidad1EsIgualACantidad2 = true;
+var cantidad2EsDiferenteACantidad2 = false;
+
+Console.WriteLine($"son {cantidad1} y {cantidad2} iguales? {cantidad1EsIgualACantidad2}");
+Console.WriteLine($"son {cantidad1} y {cantidad2} diferentes? {cantidad2EsDiferenteACantidad2}");
+
+var esCantidad1MayorQue10 = cantidad1 > 10;
+var esCantidad1MenorQue10 = cantidad1 < 10;
+
+var esCantidad1MenorOIgualque10 = cantidad1 <= 10;
+var esCantidad1MayorOIgualque10 = cantidad1 >= 10;
+
+string? nombre1 = "carlos";
+var nombre2 = "Juan";
+
+var sonlosNombres1IgualANombre2 = nombre1 == nombre2;
+
+var sonNombre1Nulo = nombre1 is null;
+var sonNombre1NoNulo = nombre1 is not null;
+
+Console.WriteLine("es nombre1 igual a nulo?" + sonNombre1Nulo );
+Console.WriteLine("es nombre1 igual a nulo?" + sonNombre1NoNulo);
+
+
+
---
+++ sintaxisBasicaDeC-/variables/Program.cs
@@ -0,0 +1,10 @@
+﻿var cantidad = 1;
+cantidad = 2;
+
+const int miConstante = 7;
+
+const double factorConversionKilometrosPorHoraAMetrosPorSegundo = 3.6;
+
+var velocidadKPH = 100;
+
+var velocidadMPS =velocidadKPH / factorConversionKilometrosPorHoraAMetrosPorSegundo;
