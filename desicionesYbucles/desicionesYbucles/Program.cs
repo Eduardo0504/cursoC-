150eb717e1b33bd5ec9562aeeadd55f9423eb851
Eduardo0504 <maujohernav50@gmail.com>
2025-06-27 11:47:41 -0600

    operador ternario

--- desicionesYbucles/desicionesYbucles/Program.cs
+++ desicionesYbucles/desicionesYbucles/Program.cs
@@ -1,14 +1,15 @@
-﻿var cantidad = 6;
-var estaEntre5y10 = cantidad > 5 && cantidad < 10; // true
+﻿var usuarioEstaLogueado = true;
 
-Console.WriteLine($"esta el {cantidad} entre 5 y 10 {estaEntre5y10}");
+string mensajeBienvenida;
 
-var cantida1Es5o6 = cantidad == 5 || cantidad == 6;
+if (usuarioEstaLogueado)
+{
+	mensajeBienvenida = "que bueno que has vuelto";
+}
+else
+{
+	mensajeBienvenida = "logueate para comenzar";
+}
+string mensajeBienvenda = usuarioEstaLogueado ? "que bueno que has vuelto¡" : "logueate para comenzar";
 
-Console.WriteLine($"¿es {cantidad} igual a 5 o 6? {cantida1Es5o6}");
-
-string? apellido = null;
-
-var estaElApellidoEnMayusculas =apellido is not null && apellido == apellido.ToUpper();
-Console.WriteLine("¿esta el string en mayusculas? " + estaElApellidoEnMayusculas);
-
+Console.WriteLine(mensajeBienvenda);
