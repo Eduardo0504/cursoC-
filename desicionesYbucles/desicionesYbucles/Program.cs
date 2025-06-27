8393a1c31c87622364e04c3eb2444aedd184cbc2
Eduardo0504 <maujohernav50@gmail.com>
2025-06-27 10:45:26 -0600

    operadores condicionales

--- desicionesYbucles/desicionesYbucles/Program.cs
+++ desicionesYbucles/desicionesYbucles/Program.cs
@@ -1,28 +1,14 @@
-﻿var cantidad1 = 5;
-var cantidad2 = 5;
+﻿var cantidad = 6;
+var estaEntre5y10 = cantidad > 5 && cantidad < 10; // true
 
-var cantidad1EsIgualACantidad2 = true;
-var cantidad2EsDiferenteACantidad2 = false;
+Console.WriteLine($"esta el {cantidad} entre 5 y 10 {estaEntre5y10}");
 
-Console.WriteLine($"son {cantidad1} y {cantidad2} iguales? {cantidad1EsIgualACantidad2}");
-Console.WriteLine($"son {cantidad1} y {cantidad2} diferentes? {cantidad2EsDiferenteACantidad2}");
+var cantida1Es5o6 = cantidad == 5 || cantidad == 6;
 
-var esCantidad1MayorQue10 = cantidad1 > 10;
-var esCantidad1MenorQue10 = cantidad1 < 10;
+Console.WriteLine($"¿es {cantidad} igual a 5 o 6? {cantida1Es5o6}");
 
-var esCantidad1MenorOIgualque10 = cantidad1 <= 10;
-var esCantidad1MayorOIgualque10 = cantidad1 >= 10;
+string? apellido = null;
 
-string? nombre1 = "carlos";
-var nombre2 = "Juan";
-
-var sonlosNombres1IgualANombre2 = nombre1 == nombre2;
-
-var sonNombre1Nulo = nombre1 is null;
-var sonNombre1NoNulo = nombre1 is not null;
-
-Console.WriteLine("es nombre1 igual a nulo?" + sonNombre1Nulo );
-Console.WriteLine("es nombre1 igual a nulo?" + sonNombre1NoNulo);
-
-
+var estaElApellidoEnMayusculas =apellido is not null && apellido == apellido.ToUpper();
+Console.WriteLine("¿esta el string en mayusculas? " + estaElApellidoEnMayusculas);
 
