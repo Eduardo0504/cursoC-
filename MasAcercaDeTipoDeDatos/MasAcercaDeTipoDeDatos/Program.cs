ea67ae6fe603acb5693e1224dbb35f156359d703
Eduardo0504 <maujohernav50@gmail.com>
2025-07-01 10:51:09 -0600

    tostring y parse

---
+++ MasAcercaDeTipoDeDatos/MasAcercaDeTipoDeDatos/Program.cs
@@ -0,0 +1,24 @@
+﻿//var cantidad1 = "5";
+
+//int cantidad = int.Parse(cantidad1);
+
+//cantidad++;
+
+//Console.WriteLine(cantidad); // esto da 6
+
+var supuestoNumero = "6";
+
+int numero;
+
+var pudoParsear = int.TryParse(supuestoNumero, out numero);
+
+if (pudoParsear)
+{
+	//lugar donde no puede usar la variable numero
+	numero++;
+	Console.WriteLine(numero);
+}
+else
+{
+	Console.WriteLine($"el valor {supuestoNumero} no tiene el formato correcto. ");
+}
