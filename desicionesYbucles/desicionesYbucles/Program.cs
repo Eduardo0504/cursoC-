9be231d7b1628ca0dcdae18bbc6dacf2c4ca169c
Eduardo0504 <maujohernav50@gmail.com>
2025-07-01 10:00:00 -0600

    bucles infinitos

--- desicionesYbucles/desicionesYbucles/Program.cs
+++ desicionesYbucles/desicionesYbucles/Program.cs
@@ -1,10 +1,43 @@
-﻿for (int i = 1; i <= 10; i++)
+﻿//while (true)
 {
-	if (i == 5)
+	//Console.WriteLine("Este codigo no se detiene");
+}
+//for(; ; )
+{
+	//Console.WriteLine("este codigo tambien no se detiene");
+}
+
+while (true)
+{
+	Console.WriteLine("te damos la bienvenida a la calculadora de sumas");
+	Console.Write("Escriba el primer numero ");
+	var entradaUsuario = Console.ReadLine();
+
+	if (entradaUsuario is null)
 	{
-		//break;
+		Console.WriteLine("vuelve a comenzar");
 		continue;
 	}
-	Console.WriteLine(i);
+
+	var numero1 = decimal.Parse(entradaUsuario);
+	Console.Write(" Escriba el segundo numero: ");
+	entradaUsuario = Console.ReadLine();
+
+	if (entradaUsuario is null)
+	{
+		Console.WriteLine("Error vuelve a comenzar");
+		continue;
+	}
+	var numero2 = decimal.Parse(entradaUsuario);
+	Console.WriteLine($"el resultado de sumar {numero1} y {numero2} es {numero1 + numero2}");
+	Console.Write("Desea sumar otra vez? (y/n) ");
+	entradaUsuario = Console.ReadLine();
+	{
+		if (entradaUsuario != "y")
+		{
+			break;
+		}
+
+	}
+	Console.WriteLine("Adios");
 }
-Console.WriteLine("fin");
