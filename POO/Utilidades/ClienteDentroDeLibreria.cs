3b1ad7d79442d5b0afc61b09a722a04584ee4173
Eduardo0504 <maujohernav50@gmail.com>
2025-07-24 16:52:07 -0600

    Internal

---
+++ POO/Utilidades/ClienteDentroDeLibreria.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilidades
+{
+	internal class ClienteDentroDeLibreria
+	{
+		public void Prueba()
+		{
+			var clasePublicaDePrueba = new ClasePublicaDePrueba();
+			clasePublicaDePrueba.PropiedadInterna = 5;
+		}
+	}
+}
