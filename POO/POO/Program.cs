76f3e3e95864160e1849a91ed65dce65996dafce
Eduardo0504 <maujohernav50@gmail.com>
2025-07-24 15:50:45 -0600

    Herencia con records

--- POO/POO/Program.cs
+++ POO/POO/Program.cs
@@ -1,33 +1,3 @@
 ﻿using POO;
 
-var carro = new Carro("Toyota");
-var carroString = carro.ToString();
-Console.WriteLine("carro tostring: " + carroString);
-
-AceptoCualquierParametro(carro);
-
-//AceptoCualquierParametro(42);
-
-//AceptoCualquierParametro("Eduardo");
-
-AceptoCualquierParametro(new Camion());
-
-void AceptoCualquierParametro(object objeto)
-{
-	Console.WriteLine("--- Inicio del Metodo--- ");
-
-
-	if (objeto is Carro carro)
-	{
-		Console.WriteLine("El objeto es un carro");
-		carro.SonarClaxon();
-	}
-	else if(objeto is Camion camion) 
-	{
-		Console.WriteLine("El objeto es un camion ");
-		camion.DarReversa();
-	}
-
-	Console.WriteLine("---Fin del Metodo--- ");
-}
-
+var profesor = new Profesor();
