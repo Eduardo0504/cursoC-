b62d6bef8e91756c3d1eeeda9199427df74d9283
Eduardo0504 <maujohernav50@gmail.com>
2025-07-28 16:11:27 -0600

    Protected

---
+++ POO/Utilidades/ClasePublicaDePrueba.cs
@@ -0,0 +1,24 @@
+﻿namespace Utilidades
+{
+	public class ClasePublicaDePrueba
+	{
+		public int PropiedadPublica { get; set; }
+		internal int PropiedadInterna { get; set; }
+		protected int PropiedadProtegida { get; set; }
+		public void MetodoPublico()
+		{
+			PropiedadInterna= 1;
+			PropiedadProtegida= 2;
+			Console.WriteLine("Soy un metodo publico de otro proyecto");
+		}
+
+		private void MetodoPrivado()
+		{
+			var claseinterna = new ClaseInterna();
+		}
+		private class claseInterna
+		{
+
+		}
+	}
+}
