897039d14d1bbc961a1b07bedde907870a21cf86
Eduardo0504 <maujohernav50@gmail.com>
2025-07-28 17:31:19 -0600

    Compartiendo Codigo de una Manera Flexible

--- POO/Utilidades/ClasePublicaDePrueba.cs
+++ POO/Utilidades/ClasePublicaDePrueba.cs
@@ -5,10 +5,13 @@
 		public int PropiedadPublica { get; set; }
 		internal int PropiedadInterna { get; set; }
 		protected int PropiedadProtegida { get; set; }
+		protected internal int PropoeidadProtectedInternal { get; set; }
+
 		public void MetodoPublico()
 		{
 			PropiedadInterna= 1;
 			PropiedadProtegida= 2;
+			PropoeidadProtectedInternal= 3;
 			Console.WriteLine("Soy un metodo publico de otro proyecto");
 		}
 
